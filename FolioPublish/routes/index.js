9650bff2711ddcb2cc779d548cda21ee0f73bb59
Julian Sandersius <sandersiusjulian@gmail.com>
2018-08-17 22:33:39 -0700

    label routes

--- FolioPublish/routes/index.js
+++ FolioPublish/routes/index.js
@@ -7,5 +7,21 @@ router.get('/', function (req, res, next) {
 });
 
 
+/* GET projects page. */
+router.get('/', function (req, res, next) {
+  res.render('projects', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/', function (req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+/* GET contact page. */
+router.get('/', function (req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+
 
 module.exports = router;
